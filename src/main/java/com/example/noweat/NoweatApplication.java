c78ba65b5cb80df3d57ed4ffa1723fee9212be63
tgg <qazplmtfgcv123@gmail.com>
2025-02-28 21:28:17 +0900

    Init : 초기 설정

---
+++ src/main/java/com/example/noweat/NoweatApplication.java
@@ -0,0 +1,13 @@
+package com.example.noweat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoweatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoweatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/noweat/NoweatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.noweat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoweatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
