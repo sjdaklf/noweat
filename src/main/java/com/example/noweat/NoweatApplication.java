0a1450362eca4abd4166d9ce0f28295a14de5830
tgg <qazplmtfgcv123@gmail.com>
2025-02-28 22:33:54 +0900

    feat : entity 생성

--- src/main/java/com/example/noweat/NoweatApplication.java
+++ src/main/java/com/example/noweat/NoweatApplication.java
@@ -2,8 +2,13 @@ package com.example.noweat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+import static org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NoweatApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/com/example/noweat/domain/user/entity/User.java
@@ -0,0 +1,43 @@
+package com.example.noweat.domain.user.entity;
+
+import com.example.noweat.domain.common.entity.BaseEntity;
+import com.example.noweat.domain.user.enums.UserRole;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+public class User extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String userAddress;
+
+    private String username;
+
+    private Long storeCount;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+
+    private boolean isDeleted;
+
+    @Builder
+    public User(String email, String password, String userAddress, String username, Long storeCount, UserRole userRole, boolean isDeleted) {
+        this.email = email;
+        this.password = password;
+        this.userAddress = userAddress;
+        this.username = username;
+        this.storeCount = storeCount;
+        this.userRole = userRole;
+        this.isDeleted = isDeleted;
+    }
+}
