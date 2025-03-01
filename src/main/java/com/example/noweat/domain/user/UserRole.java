d6ef5ff472fa244cfce6755a997501b2b9fcc09e
tgg <qazplmtfgcv123@gmail.com>
2025-03-01 17:39:52 +0900

    feat : signup 완성

--- src/main/java/com/example/noweat/domain/user/UserRole.java
+++ src/main/java/com/example/noweat/domain/user/UserRole.java
@@ -3,14 +3,17 @@ package com.example.noweat.domain.user;
 import com.sun.jdi.request.InvalidRequestStateException;
 
 import java.util.Arrays;
+import java.util.Optional;
 
 public enum UserRole {
     OWNER, USER;
 
-    public static UserRole of(String role) {
-        return Arrays.stream(UserRole.values())
-                .filter(r -> r.name().equalsIgnoreCase(role))
-                .findFirst()
-                .orElseThrow(() -> new InvalidRequestStateException("유효하지 않은 UserRole"));
+    public static Optional<UserRole> of(String role) {
+        for(UserRole userRole : UserRole.values()){
+            if(userRole.name().equals(role)){
+                return Optional.of(userRole);
+            }
+        }
+        return Optional.empty();
     }
 }
--- src/main/java/com/example/noweat/repository/user/UserRepository.java
+++ src/main/java/com/example/noweat/repository/user/UserRepository.java
@@ -3,6 +3,8 @@ package com.example.noweat.repository.user;
 import com.example.noweat.domain.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.Optional;
 
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByEmail(String email);
 }
