ed856777132c0443e57ff05e764912db05ef8d93
SeongHyeon <sng02006@naver.com>
2025-03-03 17:19:04 +0900

    feat : 유저 CR

--- src/main/java/com/example/noweat/controller/user/UserController.java
+++ src/main/java/com/example/noweat/controller/user/UserController.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
@@ -18,20 +20,20 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/users")
-    public ResponseEntity<UserResponseDto> getUser(AuthUser authUser) {
-        UserResponseDto userResponseDto = userService.getUser(authUser);
+    public ResponseEntity<UserResponseDto> findUser(AuthUser authUser) {
+        UserResponseDto userResponseDto = userService.findUser(authUser);
         return ResponseEntity.ok(userResponseDto);
     }
 
     @GetMapping("/users/stores")
-    public ResponseEntity<UserStoreResponseDto> getStoreByUser(AuthUser authUser) {
-        UserStoreResponseDto userStoreResponseDto = userService.getStoreByUser(authUser);
+    public ResponseEntity<List<UserStoreResponseDto>> findStoresByUserId(AuthUser authUser) {
+        List<UserStoreResponseDto> userStoreResponseDto = userService.findStoresByUserId(authUser);
         return ResponseEntity.ok(userStoreResponseDto);
     }
 
     @GetMapping("/users/reviews")
-    public ResponseEntity<UserReviewResponseDto> getReviewByUser(AuthUser authUser) {
-        UserReviewResponseDto userReviewResponseDto = userService.getReviewByUser(authUser);
+    public ResponseEntity<List<UserReviewResponseDto>> findReviewsByUserId(AuthUser authUser) {
+        List<UserReviewResponseDto> userReviewResponseDto = userService.findReviewsByUserId(authUser);
         return ResponseEntity.ok(userReviewResponseDto);
     }
 
--- src/main/java/com/example/noweat/repository/review/ReviewRepository.java
+++ src/main/java/com/example/noweat/repository/review/ReviewRepository.java
@@ -1,7 +1,11 @@
 package com.example.noweat.repository.review;
 
 import com.example.noweat.domain.review.Review;
+import com.example.noweat.domain.store.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    List<Review> findReviewsByUserId(Long UserId);
 }
