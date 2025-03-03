add1c2455cdd7654a0a1ad7c93bc0808bbf64802
tgg <qazplmtfgcv123@gmail.com>
2025-03-03 22:36:12 +0900

    test : AuthController 테스트 작성

--- src/test/java/com/example/noweat/controller/auth/AuthControllerTest.java
+++ src/test/java/com/example/noweat/controller/auth/AuthControllerTest.java
@@ -1,7 +1,11 @@
 package com.example.noweat.controller.auth;
 
 import com.example.noweat.domain.user.UserRole;
+import com.example.noweat.dto.auth.reponse.RefreshTokenResponseDto;
+import com.example.noweat.dto.auth.reponse.UserSigninResponseDto;
 import com.example.noweat.dto.auth.reponse.UserSignupResponseDto;
+import com.example.noweat.dto.auth.request.UserSigninRequestDto;
+import com.example.noweat.global.argumentResolver.AuthUser;
 import com.example.noweat.global.jwt.JwtUtil;
 import com.example.noweat.service.auth.AuthService;
 import org.junit.jupiter.api.DisplayName;
@@ -15,20 +19,24 @@ import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class AuthControllerTest {
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    JwtUtil jwtUtil;
 
     @MockitoBean
     AuthService authService;
@@ -38,6 +46,7 @@ class AuthControllerTest {
     void signupTest() throws Exception {
         // given
         LocalDateTime localDateTime = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
         UserSignupResponseDto responseDto = UserSignupResponseDto.builder()
                 .id(1L)
@@ -60,6 +69,72 @@ class AuthControllerTest {
                 .andExpect(jsonPath("$.id").value(responseDto.getId()))
                 .andExpect(jsonPath("$.username").value("김민재"))
                 .andExpect(jsonPath("$.userRole").value("USER"))
-                .andExpect(jsonPath("$.createdAt").value(localDateTime));
+                .andExpect(jsonPath("$.createdAt").value(dateTimeFormatter.format(localDateTime)));
+    }
+
+    @Test
+    @DisplayName("로그인 테스트")
+    void signinTest() throws Exception{
+        // given
+        UserSigninResponseDto responseDto = UserSigninResponseDto.builder()
+                .accessToken("accessToken")
+                .refreshToken("refreshToken")
+                .build();
+
+        when(authService.signinUser(any())).thenReturn(responseDto);
+
+        // when, then
+        mockMvc.perform(post("/api/signin")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding("UTF-8")
+                        .content("{\"email\" : \"scie429@gmail.com\"," +
+                                " \"password\" : \"Sukim2929@\"," +
+                                " \"deviceId\" : \"1\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").value("accessToken"))
+                .andExpect(jsonPath("$.refreshToken").value("refreshToken"));
+    }
+
+    @Test
+    @DisplayName("로그아웃 테스트")
+    void signoutTest() throws Exception {
+        // given
+        doNothing().when(authService).signout(any(), any());
+
+        String accessToken = jwtUtil.createAccessToken(1L, "email", UserRole.USER);
+
+
+        // when, then
+        mockMvc.perform(post("/api/signout")
+                        .header("Authorization", accessToken)
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content("{\"deviceId\" : \"1\"}"))
+                .andExpect(status().isOk())
+                .andExpect(content().bytes(new byte[0]));
+    }
+
+    @Test
+    @DisplayName("토큰 재발급 테스트")
+    void refreshTest() throws Exception {
+        // given
+        RefreshTokenResponseDto responseDto = RefreshTokenResponseDto.builder()
+                .accessToken("accessToken")
+                .refreshToken("refreshToken")
+                .build();
+
+        when(authService.refreshToken(any())).thenReturn(responseDto);
+
+        String refreshToken = jwtUtil.createRefreshToken(1L, "email", UserRole.USER);
+
+        // when, then
+        mockMvc.perform(post("/api/refresh")
+                .contentType(MediaType.APPLICATION_JSON)
+                .characterEncoding("UTF-8")
+                .content("{\"refreshToken\" :  \"" + refreshToken + "\"}"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").value("accessToken"))
+                .andExpect(jsonPath("$.refreshToken").value("refreshToken"));
+
     }
 }
