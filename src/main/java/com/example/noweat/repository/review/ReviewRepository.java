41cf4fafd46854d9b79b61013a739bbed6aae97b
SeongHyeon <sng02006@naver.com>
2025-03-03 18:04:13 +0900

    feat : 유저 CRUD

--- src/main/java/com/example/noweat/repository/review/ReviewRepository.java
+++ src/main/java/com/example/noweat/repository/review/ReviewRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-    List<Review> findReviewsByUserId(Long UserId);
+    List<Review> findReviewsByUserId(Long userId);
 }
--- src/main/java/com/example/noweat/repository/store/StoreRepository.java
+++ src/main/java/com/example/noweat/repository/store/StoreRepository.java
@@ -2,9 +2,12 @@ package com.example.noweat.repository.store;
 
 import com.example.noweat.domain.store.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
-    List<Store> findStoresByUserId(Long UserId);
+    @Query ("SELECT s FROM Store s WHERE s.user.id = :userId AND s.isClosed = false")
+    List<Store> findStoresByUserId(@Param("userId") Long userId);
 }
