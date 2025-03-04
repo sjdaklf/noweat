d2ce679170d9b727c1bcaff221bbebe9e25e2ccb
tgg <qazplmtfgcv123@gmail.com>
2025-03-04 22:09:56 +0900

    feat : 사장님 주문 내역 조회 작성

--- src/main/java/com/example/noweat/repository/order/OrderRepository.java
+++ src/main/java/com/example/noweat/repository/order/OrderRepository.java
@@ -3,6 +3,8 @@ package com.example.noweat.repository.order;
 import com.example.noweat.domain.order.Order;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +15,8 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
 
     @EntityGraph(attributePaths = "store")
     List<Order> findByUser_Id(Long userId);
+
+    @EntityGraph(attributePaths = "user")
+    @Query("SELECT o FROM Order o WHERE o.store.id in (SELECT s.id FROM Store s WHERE s.user.id = :userId) ORDER BY o.id")
+    List<Order> findByOwnerUserId(@Param("userId") Long userId);
 }
