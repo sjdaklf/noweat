f90da1a28ae1b1cad4c86d17970623a8c0f6c394
SeongHyeon <sng02006@naver.com>
2025-03-06 22:25:35 +0900

    feat : UserControllerTest/findStoresByUserId, findReviewsByUserId Test

--- src/main/java/com/example/noweat/dto/user/response/UserStoreResponseDto.java
+++ src/main/java/com/example/noweat/dto/user/response/UserStoreResponseDto.java
@@ -10,15 +10,15 @@ import java.time.LocalTime;
 @Getter
 public class UserStoreResponseDto {
     private final Long id;
-    private final String storeName;
+    private final String name;
     private final StoreCategory storeCategory;
     private final Long minOrderPrice;
     private final Double averageRating;
 
     @Builder
-    public UserStoreResponseDto(Long id, String storeName, StoreCategory storeCategory, Long minOrderPrice, Double averageRating) {
+    public UserStoreResponseDto(Long id, String name, StoreCategory storeCategory, Long minOrderPrice, Double averageRating) {
         this.id = id;
-        this.storeName = storeName;
+        this.name = name;
         this.storeCategory = storeCategory;
         this.minOrderPrice = minOrderPrice;
         this.averageRating = averageRating;
